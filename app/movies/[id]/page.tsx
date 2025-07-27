cf51198be25d3081feadbf9c6c74917b782fa3c6
Maxamed459 <maxamedmahdi459@gmail.com>
2025-07-27 17:43:59 +0300

    add responsiveness

--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -79,47 +79,51 @@ export default function MoviePage() {
   }, [id]);
 
   return (
-    <div className="flex items-start flex-col max-w-6xl mx-auto mt-4">
-      <button
-        className="mt-4 px-4 py-2 bg-slate-800 text-white rounded mb-4 flex items-center gap-2"
-        onClick={() => router.back()}
-      >
-        <FaArrowLeft />
-        back
-      </button>
+    <div className="bg-linear-to-r from-[#000b58] to-purple-700">
+      <div className="max-w-[90%] mx-auto flex items-start flex-col mt-4">
+        <button
+          className="mt-4 px-4 py-2 bg-purple-700  text-white rounded mb-4 flex items-center gap-2"
+          onClick={() => router.back()}
+        >
+          <FaArrowLeft />
+          back
+        </button>
 
-      <div className="flex gap-4 h-full py-4">
-        <img
-          className="w-1/3 object-fit"
-          loading="lazy"
-          src={`https://image.tmdb.org/t/p/w500${movieData?.poster_path}`}
-          alt={movieData?.original_title}
-        />
-        <div className="w-2/3">
-          <h2 className="text-3xl font-medium">{movieData?.original_title}</h2>
-          <p className="text-[15px] text-slate-700">{movieData?.overview}</p>
-          <p className="text-[15px] text-slate-700">
-            <span className="font-bold">Popularity:</span>{" "}
-            {movieData?.popularity}
-          </p>
-          <p className="text-[15px] text-slate-700">
-            <span className="font-bold">Adult:</span>{" "}
-            {movieData?.adult ? "Yes" : "No"}
-          </p>
+        <div className="flex flex-col md:flex-row gap-4 h-full py-4">
+          <img
+            className="w-[90%] md:w-1/3 object-fit"
+            loading="lazy"
+            src={`https://image.tmdb.org/t/p/w500${movieData?.poster_path}`}
+            alt={movieData?.original_title}
+          />
+          <div className="w-[90%] md:w-2/3">
+            <h2 className="text-3xl font-medium text-white">
+              {movieData?.original_title}
+            </h2>
+            <p className="text-[15px] text-gray-200">{movieData?.overview}</p>
+            <p className="text-[15px] text-gray-200">
+              <span className="font-bold">Popularity:</span>{" "}
+              {movieData?.popularity}
+            </p>
+            <p className="text-[15px] text-gray-200">
+              <span className="font-bold">Adult:</span>{" "}
+              {movieData?.adult ? "Yes" : "No"}
+            </p>
 
-          {trailer ? (
-            <iframe
-              width="100%"
-              height="500"
-              src={`https://www.youtube.com/embed/${trailer.key}`}
-              title="YouTube trailer"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              className="rounded-lg shadow-md"
-            />
-          ) : (
-            <p>No trailer available.</p>
-          )}
+            {trailer ? (
+              <iframe
+                width="100%"
+                height="500"
+                src={`https://www.youtube.com/embed/${trailer.key}`}
+                title="YouTube trailer"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                className="rounded-lg shadow-md"
+              />
+            ) : (
+              <p>No trailer available.</p>
+            )}
+          </div>
         </div>
       </div>
     </div>
