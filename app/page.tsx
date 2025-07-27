f6384a658f165e10902fa497557e97a0e23c6609
Maxamed459 <maxamedmahdi459@gmail.com>
2025-07-27 17:44:18 +0300

    add responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,31 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div>
+    <div className="flex items-center justify-center absolute inset-0 bg-linear-to-r from-[#000b58] to-purple-700">
+      <div className="max-w-[90%] mx-auto p-4 flex flex-col items-center justify-center ">
+        <div className="mb-4">
+          <h1 className="text-2xl md:text-3xl text-white lg:text-6xl font-bold text-center mb-4">
+            Welcome Next Movie App
+          </h1>
+          <p className="text-[18px] text-gray-200 text-center mb-4 max-w-2/3 mx-auto">
+            Here you can find the last movies, old movies, Entertainment movies,
+            Romantic Movies, Cartoons, and Action movies{" "}
+          </p>
+        </div>
+
+        <form className="w-3/4">
+          <div className="flex fle gap-4">
+            <input
+              type="text"
+              placeholder="Enter The name of the movie"
+              id="searchFilm"
+              className="px-6 py-3 rounded-md bg-gray-200 flex-3/4"
+            />
+            <button className="px-6 py-3 rounded-lg bg-gray-200 text-black font-medium flex-1/4 hover:bg-gray-700">
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
