2fbae9a212ecb05b4d5565456814ef9c09f13b88
Maxamed459 <maxamedmahdi459@gmail.com>
2025-07-27 17:50:36 +0300

    modified something

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,35 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="min-h-[90vh] bg-linear-to-r from-[#000b58] to-purple-700">
+      <div className="container mt-9">
+        <div className="max-w-xl mx-auto pt-20">
+          <h1 className="text-3xl lg:text-4xl text-white font-bold">
+            Welcome to the Movie App
+          </h1>
+          <p className="text-[16px] text-gray-200 leading-6 mt-4 ">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+            voluptatem tempora recusandae, quo nisi obcaecati deleniti animi
+            laboriosam, ab a, quos rem commodi nulla alias optio! Explicabo
+            eaque earum porro!Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Autem voluptatem tempora recusandae, quo nisi
+            obcaecati deleniti animi laboriosam, ab a, quos rem commodi nulla
+            alias optio! Explicabo eaque earum porro!Lorem ipsum, dolor sit amet
+            consectetur adipisicing elit. Autem voluptatem tempora recusandae,
+            quo nisi obcaecati deleniti animi laboriosam, ab a, quos rem commodi
+            nulla alias optio! Explicabo eaque earum porro!Lorem ipsum, dolor
+            sit amet consectetur adipisicing elit. Autem voluptatem tempora
+            recusandae, quo nisi obcaecati deleniti animi laboriosam, ab a, quos
+            rem commodi nulla alias optio! Explicabo eaque earum porro!Lorem
+            ipsum, dolor sit amet consectetur adipisicing elit. Autem voluptatem
+            tempora recusandae, quo nisi obcaecati deleniti animi laboriosam, ab
+            a, quos rem commodi nulla alias optio! Explicabo eaque earum porro!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
