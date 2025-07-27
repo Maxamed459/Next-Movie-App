de6fa3c19d96c3e36a814f260ca6bb3e61468111
Maxamed459 <maxamedmahdi459@gmail.com>
2025-07-27 17:50:26 +0300

    modified something

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,27 +2,31 @@ import React from "react";
 
 const page = () => {
   return (
-    <div className="container mt-9">
-      <div className="max-w-xl mx-auto pt-20">
-        <h1 className="text-xl font-bold">Welcome to the Movie App</h1>
-        <p className="text-sm text-gray-600 leading-5 mt-4 ">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
-          voluptatem tempora recusandae, quo nisi obcaecati deleniti animi
-          laboriosam, ab a, quos rem commodi nulla alias optio! Explicabo eaque
-          earum porro!Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Autem voluptatem tempora recusandae, quo nisi obcaecati deleniti animi
-          laboriosam, ab a, quos rem commodi nulla alias optio! Explicabo eaque
-          earum porro!Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Autem voluptatem tempora recusandae, quo nisi obcaecati deleniti animi
-          laboriosam, ab a, quos rem commodi nulla alias optio! Explicabo eaque
-          earum porro!Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Autem voluptatem tempora recusandae, quo nisi obcaecati deleniti animi
-          laboriosam, ab a, quos rem commodi nulla alias optio! Explicabo eaque
-          earum porro!Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-          Autem voluptatem tempora recusandae, quo nisi obcaecati deleniti animi
-          laboriosam, ab a, quos rem commodi nulla alias optio! Explicabo eaque
-          earum porro!
-        </p>
+    <div className="min-h-[90vh] bg-linear-to-r from-[#000b58] to-purple-700">
+      <div className="container mt-9">
+        <div className="max-w-xl mx-auto pt-20">
+          <h1 className="text-3xl lg:text-4xl text-white font-bold">
+            Welcome to the Movie App
+          </h1>
+          <p className="text-[16px] text-gray-200 leading-6 mt-4 ">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem
+            voluptatem tempora recusandae, quo nisi obcaecati deleniti animi
+            laboriosam, ab a, quos rem commodi nulla alias optio! Explicabo
+            eaque earum porro!Lorem ipsum, dolor sit amet consectetur
+            adipisicing elit. Autem voluptatem tempora recusandae, quo nisi
+            obcaecati deleniti animi laboriosam, ab a, quos rem commodi nulla
+            alias optio! Explicabo eaque earum porro!Lorem ipsum, dolor sit amet
+            consectetur adipisicing elit. Autem voluptatem tempora recusandae,
+            quo nisi obcaecati deleniti animi laboriosam, ab a, quos rem commodi
+            nulla alias optio! Explicabo eaque earum porro!Lorem ipsum, dolor
+            sit amet consectetur adipisicing elit. Autem voluptatem tempora
+            recusandae, quo nisi obcaecati deleniti animi laboriosam, ab a, quos
+            rem commodi nulla alias optio! Explicabo eaque earum porro!Lorem
+            ipsum, dolor sit amet consectetur adipisicing elit. Autem voluptatem
+            tempora recusandae, quo nisi obcaecati deleniti animi laboriosam, ab
+            a, quos rem commodi nulla alias optio! Explicabo eaque earum porro!
+          </p>
+        </div>
       </div>
     </div>
   );
